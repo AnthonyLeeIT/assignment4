35b2ee62fe8963471b27da5c0e07cdaceef4e818
AnthonyLeeIT <anthonyhojinlee@gmail.com>
2026-06-10 00:53:40 +0000

    Updated test_subtraction function and added parametrize tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,6 +4,7 @@ from app.operations import addition, division, multiplication, subtraction
 
 Number = Union[int, float]
 
+#Addition Tests
 @pytest.mark.parametrize( 
     "a, b, expected",
     [
@@ -24,21 +25,78 @@ Number = Union[int, float]
 
 def test_addition(a, b, expected):
     assert addition(a, b) == pytest.approx(expected)
-    
+
 #def test_addition(a, b, expected):
  #   assert addition(a, b) == expected
 
 # def test_addition():
 #    assert addition(1,1) == 2
 
-def test_subtraction():
-    assert subtraction(1,1) == 0
 
+# Subtractions Tests
+@pytest.mark.parametrize( 
+    "a, b, expected",
+    [
+        (10, 5, 5),
+        (0, 0, 0),
+        (-6, 4, -10),
+        (5.7, 4.3, 1.4),
+        (-5.7, 4.3, -10),
+    ],
+    ids=[
+        "subtract_two_positive_integers",
+        "subtract_two_zeros",
+        "subtract_negative_and_positive_integer",
+        "subtract_two_positive_floats",
+        "subtract_negative_float_and_positive_float",
+    ]                                
+)
+def test_subtraction(a, b, expected):
+    assert subtraction(a, b) == pytest.approx(expected)
+
+# Multiplication Tests
+@pytest.mark.parametrize( 
+    "a, b, expected",
+    [
+        (5, 10, 15),
+        (0, 0, 0),
+        (-6, 4, -2),
+        (5.7, 4.3, 10.0),
+        (-5.7, 4.3, -1.4),
+    ],
+    ids=[
+        "add_two_positive_integers",
+        "add_two_zeros",
+        "add_negative_and_positive_integer",
+        "add_two_positive_floats",
+        "add_negative_float_and_positive_float",
+    ]                                
+)
 def test_multiplication():
     assert multiplication(1,1) == 1
 
+# Division positive tests
+@pytest.mark.parametrize( 
+    "a, b, expected",
+    [
+        (5, 10, 15),
+        (0, 0, 0),
+        (-6, 4, -2),
+        (5.7, 4.3, 10.0),
+        (-5.7, 4.3, -1.4),
+    ],
+    ids=[
+        "add_two_positive_integers",
+        "add_two_zeros",
+        "add_negative_and_positive_integer",
+        "add_two_positive_floats",
+        "add_negative_float_and_positive_float",
+    ]                                
+)
 def test_division_positive():
     assert division(1,1) == 1
+
+# Division by zero tests
 
 def test_division_by_zero():
     """Test division by zero."""
