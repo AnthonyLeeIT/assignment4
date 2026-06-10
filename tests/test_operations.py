b518f8d6ddfb9b899f0adeb67ef9ceac36a028ec
AnthonyLeeIT <anthonyhojinlee@gmail.com>
2026-06-10 00:36:37 +0000

    Preparing mark parameterize

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,5 +1,26 @@
 import pytest
+from typing import Union
 from app.operations import addition, division, multiplication, subtraction
+
+Number = Union[int, float]
+
+@pytest.mark.parametrize( 
+    "a, b, expected",
+    [
+        (5, 10, 15),
+        (0, 0, 0),
+        (-6, 4, -2),
+        (5.7, 4.3, 10.0),
+        (-5.7, 4.3, -1.4),
+    ],
+    ids=[
+        "add_two_positive_integers",
+        "add_two_zeros",
+        "add_negative_and_positive_integer",
+        "add_two_positive_floats",
+        "add_negative_float_and_positive_float",
+    ]                                
+)
 
 def test_addition():
     assert addition(1,1) == 2
